c9d6a154051058a39cc1fded49eb9e8e27a31015
John Goh <john@accendo.com.my>
2019-10-01 19:30:34 +0800

    wip: extracting red channel mask

--- app.py
+++ app.py
@@ -31,12 +31,36 @@ def splitHSVChannels(inputBGR):
     return [red, yellow, green, cyan, blue, magenta]
 
 
+def contourTest(image):
+    hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    viewImage(hsv_img)
+    green_low = np.array([0, 50, 20])
+    green_high = np.array([30, 255, 255])
+    curr_mask = cv2.inRange(hsv_img, green_low, green_high)
+    hsv_img[curr_mask > 0] = ([10, 255, 200])
+    viewImage(hsv_img)  # 2
+
+    # RGB_again = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2RGB)
+    # gray = cv2.cvtColor(RGB_again, cv2.COLOR_RGB2GRAY)
+    # viewImage(gray)  # 3
+    # ret, threshold = cv2.threshold(gray, 90, 255, 0)
+    # viewImage(threshold)  # 4
+    # im2, contours, hierarchy = cv2.findContours(
+    #     threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+    # return image
+
+
 if __name__ == "__main__":
     print('hello openCV')
-    inputBGR = cv2.imread('images/File 2.jpg')
+    inputBGR = cv2.imread('images/File 1.jpg')
     inputBGR = cv2.resize(inputBGR, (800, 800))
+    inputBlurred = cv2.blur(inputBGR, (2, 2))
 
-    allSpacesHSV = splitHSVChannels(inputBGR)
+    # allSpacesHSV = splitHSVChannels(inputBGR)
+    # for i in range(len(allSpacesHSV)):
+    #     viewImage(allSpacesHSV[i], 'hsv')
 
-    for i in range(len(allSpacesHSV)):
-        viewImage(allSpacesHSV[i], 'hsv')
+    contourTest(inputBlurred)
+    # out = contourTest(inputBGR)
+    # viewImage(out)
