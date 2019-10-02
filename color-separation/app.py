08cd976d543319c266441959c699d171d845c41b
John Goh <john@accendo.com.my>
2019-10-02 19:09:12 +0800

    add demos of hue masking, canny edge detection

--- color-separation/app.py
+++ color-separation/app.py
@@ -1,18 +1,11 @@
 import cv2
 import numpy as np
-import os
 import math
 import matplotlib
-
-
-def viewImage(image, windowTitle="Display"):
-    cv2.imshow('{}'.format(windowTitle), image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
+from hsvMask import hsvMask
+from edgeDetect import edgeDetect
+from utils import viewImage
 
 if __name__ == "__main__":
     print('Hello OpenCV')
-    inputBGR = cv2.imread('images/File 2.jpg')
-    inputBGR = cv2.resize(inputBGR, (800, 800))
-    viewImage(inputBGR, 'Image')
+    
---
+++ color-separation/utils.py
@@ -0,0 +1,16 @@
+import cv2
+import os
+
+
+def viewImage(image, windowTitle="Display"):
+    cv2.imshow('{}'.format(windowTitle), image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def getImages(path):
+    file_list = []
+    for filename in os.listdir(path):
+        if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".bmp"):
+            file_list.append(filename)
+    return file_list
